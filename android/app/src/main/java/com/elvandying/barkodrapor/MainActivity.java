83a1cc04ce25cdc75e95a14378d1bfe6f79167ef
M.Yusuf Yildiz <muhammetyusuf2506@gmail.com>
2026-08-02 14:06:12 +0300

    Modernize Android system bar handling
    Çentikli ekranlar ve alt gezinme çubuğu için güvenli alanları yerel pencere insetleriyle uygula; eski durum çubuğu eklentisini kaldır.

--- android/app/src/main/java/com/elvandying/barkodrapor/MainActivity.java
+++ android/app/src/main/java/com/elvandying/barkodrapor/MainActivity.java
@@ -1,10 +1,10 @@
 package com.elvandying.barkodrapor;
 
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.webkit.WebView;
 
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
@@ -16,16 +16,7 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-        getWindow().setNavigationBarColor(Color.WHITE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            WindowManager.LayoutParams attributes = getWindow().getAttributes();
-            attributes.layoutInDisplayCutoutMode =
-                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-            getWindow().setAttributes(attributes);
-        }
+        WindowCompat.enableEdgeToEdge(getWindow());
 
         WindowInsetsControllerCompat insetsController =
             WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
@@ -33,5 +24,28 @@ public class MainActivity extends BridgeActivity {
         insetsController.setSystemBarsBehavior(
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         );
+
+        WebView webView = bridge.getWebView();
+        webView.setBackgroundColor(android.graphics.Color.WHITE);
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
+            Insets navigationInsets = windowInsets.getInsets(
+                WindowInsetsCompat.Type.navigationBars()
+            );
+            Insets statusBarInsets = windowInsets.getInsets(
+                WindowInsetsCompat.Type.statusBars()
+            );
+            Insets cutoutInsets = windowInsets.getInsets(
+                WindowInsetsCompat.Type.displayCutout()
+            );
+
+            view.setPadding(
+                Math.max(navigationInsets.left, cutoutInsets.left),
+                Math.max(statusBarInsets.top, cutoutInsets.top),
+                Math.max(navigationInsets.right, cutoutInsets.right),
+                Math.max(navigationInsets.bottom, cutoutInsets.bottom)
+            );
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(webView);
     }
 }
